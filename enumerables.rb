1354ccb8b7d78a5788c42113ecbe374c5e8bcae6
keddo <adu.kedir.abdurahman@gmail.com>
2020-06-24 12:12:13 +0300

    Make my select working with range

--- enumerables.rb
+++ enumerables.rb
@@ -30,18 +30,19 @@ module Enumerable
 
   def my_select
     return to_enum unless block_given?
-
-    if is_a?(Array)
+     
+    if is_a?(Hash)
+      my_hash = {}
+      my_each do |i, j|
+        my_hash.store(i, j) if yield(i, j)
+      end
+      my_hash
+    else 
       my_arr = []
       my_each do |i|
         my_arr << i if yield(i)
       end
       my_arr
-    elsif is_a?(Hash)
-      my_hash = {}
-      my_each do |i, j|
-        my_hash.store(i, j)
-      end
     end
   end
 
@@ -131,7 +132,7 @@ end
 p multiply_els([2,4,5])
 
 # hash = { name: 'kedir', last: 'Abdu' }
-# arr = [1, 2, 4, 6]
+arr = [1, 2, 4, 6]
 # 1. my_each (example test cases)
 # puts 'my_each'
 # puts '-------'
@@ -163,18 +164,20 @@ p multiply_els([2,4,5])
 # {:bacon=>:protein, :apple=>:fruit}
 
 # 3. my_select (example test cases)
-# puts 'my_select'
-# puts '-------'
-# friends = ["Sharon", "Leo", "Leila", "Brian", "Arun"]
-# p friends.my_select { |friend| friend != "Brian" }
-# h = { "a" => 100, "b" => 200, "c" => 300 }
-# p h.my_select {|k,v| k > "a"}  #=> {"b" => 200, "c" => 300}
-# p h.my_select {|k,v| v < 200}  #=> {"a" => 100}
-# arr.my_select do |i|
-#   if i % 2 == 0
-#     puts i
-#   end
-# end
+puts 'my_select'
+puts '-------'
+friends = ["Sharon", "Leo", "Leila", "Brian", "Arun"]
+p friends.my_select { |friend| friend != "Brian" }
+h = { "a" => 100, "b" => 200, "c" => 300 }
+p h.my_select {|k,v| k > "a"}  #=> {"b" => 200, "c" => 300}
+p h.my_select {|k,v| v < 200}  #=> {"a" => 100}
+arr.my_select do |i|
+  if i % 2 == 0
+    puts i
+  end
+end
+
+p (1..20).my_select {|i| i > 5}
 
 # my_count test cases
 # arr = [1,3,4,5,6,7,8, 5, 5]
