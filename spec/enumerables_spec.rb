ab5facba844c5b8f2ae9d0b4e1c6df94f2037e2b
paulzay <paulotieno2@gmail.com>
2020-07-23 13:48:02 +0300

    add test for hash for my_each

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -5,7 +5,7 @@ describe 'Enumerable' do
   let(:num_array) { [1,3,4,5,6,7,8,5,5] }
   let(:empty_array) { [] }
   let(:word_array) { ["Sharon", "Leo", "Leila", "Brian", "Arun"] }
-
+  let(:name_hash) {{ name: 'kedir', last: 'Abdu' }}
   describe '#my_each' do
     
     context 'If block is not given' do
@@ -23,7 +23,14 @@ describe 'Enumerable' do
         expect(test_array).to eql(custom_array)
       end
 
-      it "prints out the contents of the hash" do
+
+      it "it prints the contents of the hash" do
+        custom_hash = []
+        test_hash = []
+
+        name_hash.my_each { |k, v| custom_hash << [k.to_s, v.upcase]}
+        name_hash.each { |k, v| test_hash << [k.to_s, v.upcase]}
+        expect(test_hash).to eql(custom_hash)
       end
     
     end
